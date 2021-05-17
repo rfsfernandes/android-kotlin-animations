c44eb9713bb6cadcad7c8aedc1de04558f27fc4f
Rodrigo Fernandes <rodrigoserrafernandes98@gmail.com>
2021-05-17 21:49:38 +0100

    Project Creation

---
+++ LOOPUIUX/app/src/main/java/pt/rfernandes/loopuiux/MainActivity.kt
@@ -0,0 +1,18 @@
+package pt.rfernandes.loopuiux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.rfernandes.loopuiux.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ LOOPUIUX/app/src/main/java/pt/rfernandes/loopuiux/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pt.rfernandes.loopuiux.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
